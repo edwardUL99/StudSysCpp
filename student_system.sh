b657e15f1e3d834689d5b5776913d4ff0280d0a9
Edward Lynch-Milner <18222021@studentmail.ul.ie>
2020-10-17 14:25:52 +0100

    Updated script

--- student_system.sh
+++ student_system.sh
@@ -13,29 +13,47 @@ file=""
 separator=""
 adminUsername=""
 adminPass=""
+configFile="false"
 
 usage_str="student_system [-d] [database_name] (if not specified, default is student_sys) -u <user_name> -p <password> [-h] [host] (if host not specified, default is localhost) -au <admin_username> -ap <admin_password>"
 
 usage_str_file="student_system -f <file_name>"
 
 if [ "$COUNT" -eq "0" ]; then
-    echo "Enter the database name: "
-    read database
+	echo "Do you want to enter a path to a configuration file: (y/n)"
+	read choice
 
-    echo "Enter the username: "
-    read user
+	while [ $choice != "y" ] && [ $choice != "n" ];
+	do
+		echo "Enter y or n: "
+		read choice
+	done
 
-    echo "Enter the password: "
-    read -s pass
+	if [ $choice == "y" ]; then
+		configFile="true"
+		echo "Enter the file name: "
+		read file
+	else 
+		configFile="false"
+	
+    	echo "Enter the database name: "
+    	read database
 
-    echo "Enter the host: "
-    read host
+   	 	echo "Enter the username: "
+    	read user
 
-	echo "Enter the admin username: "
-	read adminUsername
+    	echo "Enter the password: "
+    	read -s pass
 
-	echo "Enter the admin password: "
-	read -s adminPass
+    	echo "Enter the host: "
+    	read host
+
+		echo "Enter the admin username: "
+		read adminUsername
+
+		echo "Enter the admin password: "
+		read -s adminPass
+	fi
 fi
 
 if [ "$COUNT" -eq "2" ]; then
@@ -46,7 +64,7 @@ if [ "$COUNT" -eq "2" ]; then
     fi
 fi
 
-if [ -z $file ]; then #if file is empty, it means there were the actual parameters passed in on command line, not a commandline
+if [ -z $file ] && [ $configFile != "true" ]; then #if file is empty, it means there were the actual parameters passed in on command line, not a commandline
    while [ "$COUNT" -gt "0" ];
    	do
     		case "$1" in
@@ -58,7 +76,7 @@ if [ -z $file ]; then #if file is empty, it means there were the actual paramete
 	     		 shift 2
 	     		 COUNT=$(expr $COUNT - 2)
 	     		;;
-        	        -p ) pass="$2"
+        	-p ) pass="$2"
 	     		 shift 2
 	     		 COUNT=$(expr $COUNT - 2)
 	     		;;
@@ -84,26 +102,36 @@ fi
 PROGRAM_LOCATION="/progs/"
 PROGRAM="studsys"
 
-if [ "$COUNT" -eq "0" ]; then
+if [ "$COUNT" -eq 0 ] && [ -z "$file" ]; then
 	#you have went through case statement so more than 1 argument was given
-	if [ -z $database ]; then
+	if [ -z "$database" ]; then
    		database="student_sys"
 	fi
 	
-	if [ -z $user ]; then
+	if [ -z "$user" ]; then
    		echo $usage_str
    		exit 1
 	fi	
 	
-	if [ -z $pass ]; then
+	if [ -z "$pass" ]; then
    		echo -n "Enter password: "
    		read -s pass	
 		echo 
 	fi
 	
-	if [ -z $host ]; then
+	if [ -z "$host" ]; then
    		host="localhost"
    	fi
+
+	if [ -z "$adminUsername" ]; then
+		echo $usage_str
+		exit 1
+	fi	
+
+	if [ -z "$adminPass" ]; then
+		echo $usage_str
+		exit 1
+	fi
 elif [ "$COUNT" -eq "2" ]; then
 	if [ -z $file ]; then
 		echo $usage_str
@@ -113,8 +141,8 @@ fi
 
 #call the program
 
-if [ "$COUNT" -eq "0" ]; then
-	#If the count is equals to 0, it means that more than 2 arguments were passed in, as the case statement was executed	
+if [ -z "$file" ]; then
+	#If the file is empty, you need to pass in arguments
 	$PROGRAM_LOCATION$PROGRAM -d $database -u $user -p $pass -h $host -au $adminUsername -ap $adminPass
 else
 	$PROGRAM_LOCATION$PROGRAM -f $file
